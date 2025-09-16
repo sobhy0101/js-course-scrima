ccd64cfa04761c4a350b7c7bd1c20381fc1fbd43
Mike Sobhy <sobhy0101@gmail.com>
2025-09-16 20:03:52 +0300

    Add dynamic lead list rendering to extension
    Introduced an unordered list element in the HTML and updated the JavaScript to render the initial leads array as list items. The input button now adds new leads from user input to the array, and the list is displayed on the page.

--- practice/apps/learn-basics-build-a-chrome-extension/index.js
+++ practice/apps/learn-basics-build-a-chrome-extension/index.js
@@ -1,14 +1,17 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+// 2. Grab the unordered list and store it in a const variable called ulEl
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push("www.awesomelead.com", "www.google.com", "www.facebook.com")
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
 
 ////////////////////////////////////////////////////////////////
 
