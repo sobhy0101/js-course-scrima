ab73531805435643d5835cab686bd30864f3d6e3
Mike Sobhy <sobhy0101@gmail.com>
2025-09-16 17:29:29 +0300

    Add button event to push leads and log array
    Introduced an event listener on the input button to push sample URLs into the myLeads array and log the result. Also uncommented and updated variable declarations for clarity in the commented code section.

--- practice/apps/learn-basics-build-a-chrome-extension/index.js
+++ practice/apps/learn-basics-build-a-chrome-extension/index.js
@@ -1,27 +1,33 @@
-// let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
-// inputBtn.addEventListener("click", function() {
-//     console.log("Button clicked from addEventListener")
-// })
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com", "www.google.com", "www.facebook.com")
+    console.log(myLeads)
+})
+
 
 ////////////////////////////////////////////////////////////////
 
-// If possible, use const. If not, use let.
+// // If possible, use const. If not, use let.
 
-// Which variables below should be changed from let to const?
+// // Which variables below should be changed from let to const?
 
-// The customer wants to order some stuff. Here are the details:
-let basePrice = 520
-let discount = 120
-let shippingCost = 12
-let shippingTime = "5-12 days"
+// // The customer wants to order some stuff. Here are the details:
+// const basePrice = 520
+// const discount = 120
+// let shippingCost = 12
+// let shippingTime = "5-12 days"
 
-// Whops! Turns out the shipping will be a bit more complex
-shippingCost = 15
-shippingTime = "7-14 days"
+// // Whops! Turns out the shipping will be a bit more complex
+// shippingCost = 15
+// shippingTime = "7-14 days"
 
-// Calculating the full price
-let fullPrice = basePrice - discount + shippingCost
+// // Calculating the full price
+// const fullPrice = basePrice - discount + shippingCost
 
-// Finally, notifying the customer
-console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
+// // Finally, notifying the customer
+// console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
