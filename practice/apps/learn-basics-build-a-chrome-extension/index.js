24e5c675e83363c64ce363845bd3a8888f38b3b2
Mike Sobhy <sobhy0101@gmail.com>
2025-09-17 19:52:27 +0300

    Refactor list rendering logic in Chrome extension
    Replaced direct ulEl.innerHTML updates with accumulation in a listItems variable for improved rendering of leads. Added comments and quiz instructions to clarify the process and included examples for maintaining previous HTML content when updating innerHTML.

--- practice/apps/learn-basics-build-a-chrome-extension/index.js
+++ practice/apps/learn-basics-build-a-chrome-extension/index.js
@@ -8,10 +8,28 @@ inputBtn.addEventListener("click", function() {
     console.log(myLeads)
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+// for (let i = 0; i < myLeads.length; i++) {
+//     // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+//     // create element
+//     // set text content
+//     // append to ul
+//     const listedItem = document.createElement("li") // Using createElement method to create a new li element instead of using innerHTML
+//     listedItem.textContent = myLeads[i] // Setting the text content of the li element to the current lead qarray item
+//     ulEl.append(listedItem) // Appending the li element to the ul element using the append method
+// }
 
+// New Quiz:
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+    listItems += "<li>" + myLeads[i] + "</li>"
+    console.log(listItems)
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems
+
 
 
 
@@ -44,3 +62,17 @@ for (let i = 0; i < myLeads.length; i++) {
 
 // container.innerHTML = "<button>Buy!</button>" // adds a button inside the container div
 // ////////////////////////////////////////////////////////////////
+
+// // To maintain the previous HTML and add new HTML, use += instead of = | Otherwise, it will overwrite the previous HTML
+// 
+// const container = document.getElementById("container")
+
+// container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// // When clicked, render a paragraph under the button (in the container)
+// // that says "Thank you for buying!"
+
+// function buy() {
+//     container.innerHTML = "<button onclick='buy()'>Buy!</button>" + "<p>Thank you for buying!</p>" // With this code, the button will remain after clicking it because we are overwriting the entire innerHTML of the container div
+//     container.innerHTML += "<p>Thank you for buying!</p>" // With this code, the button will disappear after clicking it because we are adding a new paragraph to the existing innerHTML of the container div
+//}
