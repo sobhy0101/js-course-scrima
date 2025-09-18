e09b1c4012e54c34159fb09cad63e3bda90af72c
Mike Sobhy <sobhy0101@gmail.com>
2025-09-18 17:15:01 +0300

    Add template strings quiz example
    Introduced a code snippet demonstrating the use of template strings for constructing an email message, including variables for recipient and sender.

--- practice/apps/learn-basics-build-a-chrome-extension/index.js
+++ practice/apps/learn-basics-build-a-chrome-extension/index.js
@@ -33,6 +33,17 @@ function renderLeads() {
     ulEl.innerHTML = listItems  
 }
 
+// ///////////// Template Strings Quiz /////////////
+
+// template strings/literals
+
+const recipient = "James"
+const sender = "Mike"
+
+// Refactor the email string to use template strings
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+
+console.log(email)
 
 ////////////////////////////////////////////////////////////////
 
