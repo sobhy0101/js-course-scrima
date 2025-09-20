6638d7811c0f3a5c14496e83861da51f21b66dd8
Mike Sobhy <sobhy0101@gmail.com>
2025-09-20 13:46:44 +0300

    Refactor myLeads handling and add array operations
    Changed myLeads initialization to a string, then parsed it to an array, pushed a new value, and converted it back to a string. Added steps to demonstrate JSON parsing/stringifying and verified the type with console.log.

--- practice/apps/learn-basics-build-a-chrome-extension/index.js
+++ practice/apps/learn-basics-build-a-chrome-extension/index.js
@@ -1,25 +1,21 @@
-let myLeads = ["www.awesomelead.com"] // Use this array to test the code below
 
-myLeads = JSON.stringify(myLeads) // converts the array into a string
+let myLeads = `["www.awesomelead.com"]`
 
-console.log(typeof myLeads) // Log out the type of myLeads
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+myLeads.push("www.lead2.com")
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 ///////////////////  Local Storage ////////////////////////
-// Practice using localStorage
-// To test it out, open the browser console and type localStorage to see what is stored in localStorage or else go to Application tab > Local Storage
-// 1. Save a key-value pair in localStorage
-// 2. Refresh the page. Get the value and log it to the console
-// 3. Clear localStorage
 
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
 
 localStorage.setItem("My Leads", "www.google.com") // saves the key-value pair in localStorage
 let name = localStorage.getItem("My Leads") // retrieves the value from localStorage using the key
