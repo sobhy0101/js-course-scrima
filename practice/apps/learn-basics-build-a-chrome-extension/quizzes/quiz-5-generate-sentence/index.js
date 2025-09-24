1039662bbb36fbe4318aeadf763bc30abbfe7929
Mike Sobhy <sobhy0101@gmail.com>
2025-09-24 14:55:05 +0300

    Add quiz 5: generate sentence Chrome extension example
    Introduces a new quiz project for learning Chrome extension basics. Includes source files, README, Webpack config, and example code for generating sentences from array data.

---
+++ practice/apps/learn-basics-build-a-chrome-extension/quizzes/quiz-5-generate-sentence/index.js
@@ -0,0 +1,24 @@
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+
+let largestCountries = ["China", "India", "USA"]
+
+let bestFruits = ["Apples", "Bananas"]
+
+function generateSentence(desc, arr) {
+    
+        // console.log(desc[i])
+        return `The ${arr.length} ${desc} are ${arr.join(", ")}`
+    }
+
+
+console.log(generateSentence("largest countries", ["China", "India", "USA"]))
+console.log(generateSentence("best fruits", ["Apples, Bannas"]))
